746aa95e08b9fbfcabac26b55fae1d8bafd7b300
Kay-Uwe Schreiner <ks@ksware.de>
2019-09-03 16:26:34 +0200

    VisibilityBinding

---
+++ src/KsWare.Presentation.ViewFramework.Common.Tests/LibTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace KsWare.Presentation.ViewFramework.Tests {
+
+	[TestFixture]
+	public class LibTests {
+
+		[Test]
+		public void NamespaceMustMatchAssemblyName() {
+			var t = typeof (KsWare.Presentation.ViewFramework.Common.AssemblyInfo);
+			var assemblyName = KsWare.Presentation.ViewFramework.Common.AssemblyInfo.Assembly.GetName(false).Name;
+			Assert.That(t.Namespace, Is.EqualTo(assemblyName));
+		}
+	}
+}
